a1e1d53e4fdb374a9fbba528f0ec681a4407012e
Bren00x <Bren00x@users.noreply.github.com>
2018-03-12 18:14:09 -0400

    Fixed Wrong URLs
    Script was using local URL and not CDN url.

--- embed.js
+++ embed.js
@@ -1,12 +1,12 @@
-    var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    link.rel  = 'stylesheet';
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'http://127.0.0.1:51098/embed.css';
+    link.href = 'https://cdn.jsdelivr.net/gh/bren00x/vocab-grabber/embed.css';
     link.media = 'all';
     head.appendChild(link);
 
-document.body.innerHTML = document.body.innerHTML + `<div class="vl-app">
+    document.body.innerHTML = document.body.innerHTML + `<div class="vl-app">
     <p class="vl-title">Vocab Grabber</p>
     <p>Vocab Grabber lets you get a full list of vocab words from <a href="https://vocabulary.com" target="_blank">vocabulary.com</a> in the form of a Word or Text Document.</p>
     <button class="vl-button" onclick="vl()">Get</button>
@@ -14,14 +14,14 @@ document.body.innerHTML = document.body.innerHTML + `<div class="vl-app">
 </div>`;
 
 
-var x = document.getElementsByClassName("entry");
-var y = document.getElementById("vl_text");
+    var x = document.getElementsByClassName("entry");
+    var y = document.getElementById("vl_text");
 
     function vl() {
         var full = "";
         for (var i = 0, len = x.length; i < len; i++) {
             //console.log(len + " " + i);
-            full += i+1 +". "+jsUcfirst(x[i].getElementsByClassName("word")[0].innerText) + " - " + x[i].getElementsByClassName("definition")[0].innerText + ".\n";
+            full += i + 1 + ". " + jsUcfirst(x[i].getElementsByClassName("word")[0].innerText) + " - " + x[i].getElementsByClassName("definition")[0].innerText + ".\n";
             if (len - 1 == i) {
                 console.log(full);
                 y.innerHTML = full
